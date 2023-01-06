345ee6bff017242d3248651b91400e9c6dd6c01c
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-06 21:52:56 +0800

    fix pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import { About, Home, Products } from "./pages";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
