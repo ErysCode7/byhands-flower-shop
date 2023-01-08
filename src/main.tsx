9ad0cee22c7fb8079a7b0547c03cdf24ad40f68c
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-08 15:07:15 +0800

    add context

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ShoppingCartProvider>
+        <App />
+      </ShoppingCartProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
