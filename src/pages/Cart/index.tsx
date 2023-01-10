50ed4d0443a5bcd5d1d32b578caff7437f3d86e8
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-10 20:32:37 +0800

    fix layout

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -9,7 +9,7 @@ const Cart = (props: Props) => {
   if (cartItems.length < 1) {
     return (
       <div className="flex flex-col items-center justify-center">
-        <h2 className="font-bold text-3xl md:text-4xl laptop:text-5xl xl:text-6xl mb-5 text-[#333]">
+        <h2 className="mt-28 md:mt-40 font-bold text-3xl md:text-4xl laptop:text-5xl xl:text-6xl mb-5 text-[#333]">
           Your cart is empty
         </h2>
         <button className="uppercase text-base md:text-lg tracking-wider mt-1 bg-[#111] text-white py-2 px-3 rounded active:scale-[0.98]">
