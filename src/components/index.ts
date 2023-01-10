c6a36cb2682460a0c20cae8062a32c5466a36f1c
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-10 21:18:37 +0800

    fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
 export { default as Footer } from "./Footer/";
 export { default as Header } from "./Header/";
 export { default as Hero } from "./Hero/";
-export { default as Product } from "./Product/";
-export { default as ProductDetails } from "./ProductDetails";
-export { default as Products } from "./Products/";
-export { default as SearchProducts } from "./SearchProducts/";
+export { default as Product } from "./ProductsPage/Product";
+export { default as ProductDetails } from "./ProductsPage/ProductDetails";
+export { default as Products } from "./ProductsPage/Products";
+export { default as SearchProducts } from "./ProductsPage/SearchProducts";
 export { default as Story } from "./Story/";
