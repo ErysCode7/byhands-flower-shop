aab2a26fc966353757c1fdc13e040d8daa185d31
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-10 20:31:10 +0800

    fix footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = (props: Props) => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-100 w-full h-[80px] flex items-center justify-center mt-10">
+    <footer className="bg-gray-100 w-full h-[80px] flex items-center justify-center mt-10 absolute bottom-0">
       <h3 className="text-[#333] text-base">
         © {year} ByHands All rights reserved
       </h3>
