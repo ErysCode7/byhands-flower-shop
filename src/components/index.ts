e41a63e5020d2c7268c41a4ef468b3565268ddef
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-10 21:19:22 +0800

    fix folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
-export { default as Footer } from "./Footer/";
-export { default as Header } from "./Header/";
-export { default as Hero } from "./Hero/";
+export { default as Story } from "./AboutPage/Story";
+export { default as Footer } from "./common/Footer";
+export { default as Header } from "./common/Header";
+export { default as Hero } from "./HomePage/Hero";
 export { default as Product } from "./ProductsPage/Product";
 export { default as ProductDetails } from "./ProductsPage/ProductDetails";
 export { default as Products } from "./ProductsPage/Products";
 export { default as SearchProducts } from "./ProductsPage/SearchProducts";
-export { default as Story } from "./Story/";
