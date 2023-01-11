3174d3142aa723675b7eada44c5003c16adf67e4
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-11 21:44:00 +0800

    added featured

---
+++ src/components/HomePage/FeaturedProducts/index.tsx
@@ -0,0 +1,13 @@
+type Props = {};
+
+const FeaturedProducts = (props: Props) => {
+  return (
+    <section>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedProducts;
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export { default as BreadCrumbs } from "./common/BreadCrumbs";
 export { default as Header } from "./common/Header";
 export { default as NotFound } from "./common/NotFound";
 export { default as Hero } from "./HomePage/Hero";
+export { default as FeaturedProducts } from "./HomePage/FeaturedProducts";
 export { default as Product } from "./ProductsPage/Product";
 export { default as ProductDetails } from "./ProductsPage/ProductDetails";
 export { default as Products } from "./ProductsPage/Products";
