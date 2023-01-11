c95604ef4f6242e2806c711033c80588b6903f10
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-11 21:39:10 +0800

    fix

--- src/components/common/NotFound/index.tsx
+++ src/components/common/NotFound/index.tsx
@@ -3,7 +3,7 @@ type Props = {};
 const Notfound = (props: Props) => {
   return (
     <main className="h-[50vh]">
-      <h2 className="mt-28 md:mt-40 text-center font-bold text-3xl md:text-4xl laptop:text-5xl xl:text-6xl mb-5 text-[#333]">
+      <h2 className="mt-28 md:mt-40 text-center font-bold text-3xl md:text-4xl laptop:text-5xl xl:text-6xl mb-5 text-main">
         404 Page not found!
       </h2>
     </main>
