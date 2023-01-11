5c2dfd2fdf046e6ab29feebe34cfdd069dda0b60
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-11 21:48:16 +0800

    fix path

--- src/components/HomePage/FeaturedProducts/index.tsx
+++ src/components/HomePage/FeaturedProducts/index.tsx
@@ -1,9 +1,18 @@
+import items from "../../../data/data.json";
+
 type Props = {};
 
 const FeaturedProducts = (props: Props) => {
   return (
     <section>
-      <div></div>
+      <div className="flex items-center">
+        {items.slice(0, 3).map((item) => (
+          <div>
+            <h3>{item.name}</h3>
+            <img src={item.imgUrl} alt="" className="w-full" />
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
