48d30bfae558566d4403f10788edf4c435f4a249
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-11 21:41:49 +0800

    fix breadcrumbs

--- src/components/common/BreadCrumbs/index.tsx
+++ src/components/common/BreadCrumbs/index.tsx
@@ -10,7 +10,8 @@ const BreadCrumbs = (props: Props) => {
     <div className="h-28 w-full bg-gray-100 flex items-center capitalize">
       <div className="w-4/5 m-auto py-5">
         <h3 className="text-xl md:text-2xl font-bold lg:text-4xl text-main">
-          <Link to="/">Home</Link> / {pathname.slice(1)}
+          <Link to="/">Home</Link> /{" "}
+          <span className="text-gray-500">{pathname.slice(1)}</span>
         </h3>
       </div>
     </div>
