e37fe0cdb7e49538677eea7d4a8d625710c67b14
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-14 23:16:51 +0800

    fix

--- src/components/ProductsPage/Product/index.tsx
+++ src/components/ProductsPage/Product/index.tsx
@@ -8,9 +8,17 @@ type Props = {
 
 const Product = ({ category, id, imgUrl, name, price }: Props) => {
   return (
-    <div className="w-[250px] h-[250px] rounded">
-      <img src={imgUrl} alt={name} className="object-cover h-full w-full" />
-      <p>{name}</p>
+    <div className="rounded py-1 h-[300px] w-full my-10 bg-gray-100">
+      <img src={imgUrl} alt={name} className="w-full h-full" />
+      <div className="flex items-center justify-between pt-2">
+        <p>{name}</p>
+        <p>{price}</p>
+      </div>
+      <div className="text-center mt-2">
+        <button className="bg-blue-500 text-white py-1 px-2 rounded w-full">
+          Add to cart
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/ProductsPage/Products/index.tsx
+++ src/components/ProductsPage/Products/index.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const Products = (props: Props) => {
   return (
     <div>
-      <div className="w-[90%] lg:w-4/5 m-auto grid grid-cols-3 gap-5 py-10 place-items-center">
+      <div className="w-[90%] lg:w-4/5 m-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 py-10 place-items-center">
         {items.map((item) => (
           <Product key={item.id} {...item} />
         ))}
