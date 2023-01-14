7c3f68d074690793d190047f86548c27f8f12e95
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-14 22:07:40 +0800

    fix

--- src/components/HomePage/Insights/index.tsx
+++ src/components/HomePage/Insights/index.tsx
@@ -1,3 +1,5 @@
+import { FaHistory } from "react-icons/fa";
+import { GiDiamondHard } from "react-icons/gi";
 import { SlCompass } from "react-icons/sl";
 
 type Props = {};
@@ -11,22 +13,25 @@ const Insights = (props: Props) => {
     },
     {
       id: 3,
-      icon: <SlCompass />,
+      icon: <GiDiamondHard />,
       text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
     },
     {
       id: 3,
-      icon: <SlCompass />,
+      icon: <FaHistory />,
       text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
     },
   ];
 
   return (
     <section className="bg-gray-300 my-3">
-      <div className="w-[90%] m-auto py-5">
-        <div>
-          <h3>Custom Furniture Built Only For You</h3>
-          <p>
+      <div className="w-[90%] lg:w-4/5 m-auto py-5">
+        <div className="lg:flex lg:items-center lg:justify-between">
+          <div className="flex flex-col text-main text-xl md:text-2xl lg:text-3xl font-bold xl:text-4xl mb-5">
+            <h3>Custom Furniture</h3>
+            <h3>Built Only For You</h3>
+          </div>
+          <p className="lg:w-[500px]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
             dolorum debitis consectetur reprehenderit non aliquam voluptates
             dolore aut vero consequuntur.
