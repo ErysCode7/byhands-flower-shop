6199aa47b3c868e9f72224e40d01fae601c627f0
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-14 21:58:01 +0800

    fix

--- src/components/HomePage/Insights/index.tsx
+++ src/components/HomePage/Insights/index.tsx
@@ -1,10 +1,37 @@
+import { SlCompass } from "react-icons/sl";
+
 type Props = {};
 
 const Insights = (props: Props) => {
+  const cardsData = [
+    {
+      id: 1,
+      icon: <SlCompass />,
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+    },
+    {
+      id: 3,
+      icon: <SlCompass />,
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+    },
+    {
+      id: 3,
+      icon: <SlCompass />,
+      text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi",
+    },
+  ];
+
   return (
-    <section>
-      <div>
-        <h3>Custom Furniture Built Only For You</h3>
+    <section className="bg-gray-300 my-3">
+      <div className="w-[90%] m-auto py-5">
+        <div>
+          <h3>Custom Furniture Built Only For You</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            dolorum debitis consectetur reprehenderit non aliquam voluptates
+            dolore aut vero consequuntur.
+          </p>
+        </div>
       </div>
     </section>
   );
