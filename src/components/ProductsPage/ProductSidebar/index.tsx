8eb232feaed2315fe5271d31fb77a40190021791
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2023-01-28 21:53:18 +0800

    fix navbar

--- src/components/ProductsPage/ProductSidebar/index.tsx
+++ src/components/ProductsPage/ProductSidebar/index.tsx
@@ -1,14 +1,22 @@
 type Props = {};
 
 const ProductSidebar = (props: Props) => {
-  const categories = ["books", "flowers", "gadgets"];
+  const categories = [
+    "books",
+    "flowers",
+    "gadgets",
+    "furniture",
+    "bags",
+    "shoes",
+    "caps",
+  ];
 
   return (
-    <div className="h-screen w-56">
-      <h3 className="text-main font-bold">Category</h3>
+    <div className="relative pr-10 md:pr-20 lg:pr-28">
+      <h3 className="text-main font-bold text-xl mb-2">Category</h3>
       {categories.map((category, idx) => (
-        <div key={idx}>
-          <h4>{category}</h4>
+        <div key={idx} className="capitalize py-1">
+          <h4 className="text-secondary cursor-pointer">{category}</h4>
         </div>
       ))}
     </div>
--- src/components/ProductsPage/Products/index.tsx
+++ src/components/ProductsPage/Products/index.tsx
@@ -1,3 +1,5 @@
+import { AiTwotoneAppstore } from "react-icons/ai";
+import { MdFormatLineSpacing } from "react-icons/md";
 import items from "../../../data/data.json";
 import Product from "../Product";
 import ProductSidebar from "../ProductSidebar";
@@ -6,13 +8,21 @@ type Props = {};
 
 const Products = (props: Props) => {
   return (
-    <div className="w-[90%] m-auto">
-      <div className="flex justify-between mt-[50px]">
+    <div className="w-[90%] lg:w-[85%] m-auto">
+      <div className="flex mt-[40px] lg:mt-[50px]">
         <ProductSidebar />
-        <div className="">
-          {/* {items.map((item) => (
-            <Product key={item.id} {...item} />
-          ))} */}
+        <div className="flex flex-col">
+          <div className="flex items-center">
+            <div className="hidden lg:flex items-center">
+              <AiTwotoneAppstore />
+              <MdFormatLineSpacing />
+            </div>
+          </div>
+          <div className="flex flex-wrap items-center justify-between">
+            {items.map((item) => (
+              <Product key={item.id} {...item} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
